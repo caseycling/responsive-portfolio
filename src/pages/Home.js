660f691b869c7736bafe99265f8efb603302cdb3
Casey <caseyclinga@gmail.com>
2020-08-25 15:20:19 -0700

    Implemented  react strap components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import NavBar from '../components/Navbar/Navbar';
+import About from '../components/About/About';
+import Carousel from '../components/Carousel/Carousel';
+import Contact from '../components/Contact/Contact';
+
+import './Home.css';
 
 function Home() {
   return (
     <div className='home'>
       <NavBar />
+      <About />
+      <Carousel />
+      <Contact />
     </div>
   );
 }
