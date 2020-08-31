f8b0c8cfe60fc252188e4be1f64a4750607eece2
Casey <caseyclinga@gmail.com>
2020-08-31 10:20:46 -0700

    Added github and linked in logo's

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 
+import github from '../../images/githubLogo.png';
+import linkedin from '../../images/linkeInLogo.png';
+
 import './About.css';
 
 const About = () => {
@@ -10,6 +13,12 @@ const About = () => {
         <div className='content'>
           <h1>Casey Clingaman</h1>
           <h5>Full-Stack Developer</h5>
+          <a href='https://github.com/caseycling/'>
+            <img src={github} alt='github' />
+          </a>
+          <a href='https://www.linkedin.com/in/casey-clingaman-b670b5177/'>
+            <img src={linkedin} alt='linkedin' />
+          </a>
           <p>
             This is a simple hero unit, a simple jumbotron-style component for
             calling extra attention to featured content or information.
