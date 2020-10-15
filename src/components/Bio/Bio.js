766de6097b0bd4ac4b7e6a34d27075c0acfe7978
Casey <caseyclinga@gmail.com>
2020-10-15 14:36:25 -0700

    Fully responsive bio component

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Container, Row, Col } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import profile from '../../images/profilePic.jpg';
 
 import './Bio.css';
@@ -8,22 +8,25 @@ const Bio = () => {
   return (
     <div className='wrapper'>
       <div className='container' id='about'>
-        <Image className='bio-pic' src={profile} alt='casey' fluid />
-        <h2>About Me</h2>
+        <Image className='bio-pic' src={profile} alt='casey' />
+        <h2>
+          <strong>About Me</strong>
+        </h2>
         <p>
           I am a full-stack web developer, passionate about turning ideas into
-          reality. Experienced with front-end and back-end development, I enjoy
-          the logical puzzles that come with software engineering and take pride
-          in writting elegant, dry code. I am excited to apply the skills I've
-          built as a developer in a challenging environment where I can grow as
-          a professional and be a positive impact as a team member. <br />
-          <br></br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          reality. I enjoy the logical puzzles that come with software
+          engineering and take pride in writting elegant, dry code. I am excited
+          to apply the skills I've built as a developer in a challenging
+          environment where I can grow as a professional and be a positive
+          impact as a team member.
+        </p>
+        <p class='sm-hide'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          pariatur. Excepteur sint occaecat cupidatat non proident.
         </p>
       </div>
     </div>
