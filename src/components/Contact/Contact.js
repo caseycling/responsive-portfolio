e1ab12c8eba15f40f94721febd90d1e132370a53
Casey <caseyclinga@gmail.com>
2020-10-21 21:21:02 -0700

    Added HTML form endpoint

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from '@formcarry/react';
+
 import './Contact.css';
 
 function Contact() {
+  const [subMsg, setSubMsg] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  // Call the `useForm` hook in your function component
+  const { state, submit } = useForm({
+    id: '4kCmNqIcz-'
+  })
+
+  function showMsg() {
+    setSubMsg(true);
+    setName('');
+    setEmail('');
+    setMessage('');
+    setTimeout(() => setSubMsg(false), 4000);
+  }
+
   return (
     <div className='contact-cont'>
       <div className='contact'>
@@ -10,29 +31,48 @@ function Contact() {
         </h1>
         <form
           id='contact-form'
-          // onSubmit={this.handleSubmit.bind(this)}
+          onSubmit={submit}
           method='POST'
         >
           <div className='form-group'>
             <label htmlFor='name'>Name</label>
-            <input type='text' className='form-control' />
+            <input
+              id='name'
+              type='text'
+              name='name'
+              value={name}
+              onChange={event => setName(event.target.value)}
+              className='form-control' />
           </div>
           <div className='form-group'>
             <label htmlFor='exampleInputEmail1'>Email address</label>
             <input
+              id='email'
               type='email'
+              name='email'
+              value={email}
+              onChange={event => setEmail(event.target.value)}
               className='form-control'
-              aria-describedby='emailHelp'
             />
           </div>
           <div className='form-group'>
             <label htmlFor='message'>Message</label>
-            <textarea className='form-control' rows='5'></textarea>
+            <textarea
+              id='message'
+              name='message'
+              value={message}
+              onChange={event => setMessage(event.target.value)}
+              className='form-control'
+              rows='5'>
+            </textarea>
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button type='submit' className='btn btn-primary' onClick={showMsg}>
             Submit
           </button>
         </form>
+        {subMsg ?
+          <div className='sub-msg'>Thank you! We received your submission</div>
+          : null}
       </div>
 
       <div className='email'>
@@ -44,7 +84,7 @@ function Contact() {
         </h2>
         <h3>caseyclinga@gmail.com</h3>
       </div>
-    </div>
+    </div >
   );
 }
 
