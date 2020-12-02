654616d3e40cf061467e71f0e1201840571e7e25
Casey <caseyclinga@gmail.com>
2020-12-01 16:28:12 -0800

    Fixed bug with button

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 
-
 import './About.css';
 
 const About = () => {
@@ -13,14 +12,9 @@ const About = () => {
           <h1>CASEY CLINGAMAN</h1>
           <h5>FULL-STACK DEVELOPER </h5>
           <h5>BASED OUT OF SEATTLE, WA</h5>
-          <Button variant='primary'>
-            <Link to='about'
-              smooth={true}
-              offset={-90}
-              duration={500}>
-              Learn more
-            </Link>
-          </Button>
+          <Link to='about' smooth={true} offset={-90} duration={500}>
+            <Button variant='primary'>Learn more</Button>
+          </Link>
         </div>
       </Jumbotron>
     </div>
