9907674fbc15c0c9676ddb4beea91ff228e897e9
Casey <caseyclinga@gmail.com>
2021-07-15 16:05:31 -0700

    Fixed font size

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,9 @@ const About = () => {
     <div className='about-cont'>
       <Jumbotron className='jumbotron' id='home'>
         <div className='content'>
-          <h1>CASEY CLINGAMAN</h1>
-          <h5>FULL-STACK DEVELOPER </h5>
-          <h5>BASED OUT OF SEATTLE, WA</h5>
+          <h1>Casey Clingaman</h1>
+          <h5>Full-Stack Developer </h5>
+          <h5>based out of Seattle, WA</h5>
           <Link to='about' smooth={true} offset={-90} duration={500}>
             <Button variant='primary'>Learn more</Button>
           </Link>
